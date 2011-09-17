6e77b46c9931fcfa98914361f83a17f59721c936
ForNeVeR <neverthness@gmail.com>
2011-09-17 21:48:29 +0700

    Gateman: removed some unused using directives.

--- Gateman/Storage.cs
+++ Gateman/Storage.cs
@@ -21,13 +21,9 @@
  */
 
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.SQLite;
-using System.IO;
-using System.Linq;
-using System.Text;
 using Hell.FirstCircle;
 
 namespace Hell.Gateman
