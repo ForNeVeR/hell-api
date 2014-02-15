0191ff0eb9f2ae2c0229dc5ab53071bed52cc992
ForNeVeR <neverthness@gmail.com>
2014-02-15 22:34:21 +0700

    Tabify HellManager.cs. Fix bug when unloading the plugin without opening options page.

--- HellManager/HellManager.cs
+++ HellManager/HellManager.cs
@@ -28,131 +28,134 @@ using Hell.FirstCircle;
 
 namespace Hell
 {
-    /// <summary>
-    /// HellOptions is plugin implementing options page for HellPlugin adapter.
-    /// </summary>
-    public class HellManager : Plugin
-    {
-        /// <summary>
-        /// List of all currently loaded plugin types.
-        /// </summary>
-        public List<Type> LoadedTypes;
+	/// <summary>
+	/// HellOptions is plugin implementing options page for HellPlugin adapter.
+	/// </summary>
+	public class HellManager : Plugin
+	{
+		/// <summary>
+		/// List of all currently loaded plugin types.
+		/// </summary>
+		public List<Type> LoadedTypes;
 
-        /// <summary>
-        /// List of all currently unloaded plugin types.
-        /// </summary>
-        public List<Type> UnloadedTypes;
+		/// <summary>
+		/// List of all currently unloaded plugin types.
+		/// </summary>
+		public List<Type> UnloadedTypes;
 
-        /// <summary>
-        /// Reference to options page.
-        /// </summary>
-        private OptionsPage options;
+		/// <summary>
+		/// Reference to options page.
+		/// </summary>
+		private OptionsPage options;
 
-        /// <summary>
-        /// Miranda database.
-        /// </summary>
-        internal DatabaseConnector Database { get; private set; }
+		/// <summary>
+		/// Miranda database.
+		/// </summary>
+		internal DatabaseConnector Database { get; private set; }
 
-        /// <summary>
-        /// List of loaded plugin instances.
-        /// </summary>
-        private List<Plugin> loadedPlugins = new List<Plugin>();
+		/// <summary>
+		/// List of loaded plugin instances.
+		/// </summary>
+		private List<Plugin> loadedPlugins = new List<Plugin>();
 
-        /// <summary>
-        /// Manager constructor.
-        /// </summary>
-        /// <param name="detectedPlugins">
-        /// List of all detected loadable plugin types.
-        /// </param>
-        public HellManager(List<Type> detectedPlugins)
-        {
-            UnloadedTypes = new List<Type>(detectedPlugins);
-        }
+		/// <summary>
+		/// Manager constructor.
+		/// </summary>
+		/// <param name="detectedPlugins">
+		/// List of all detected loadable plugin types.
+		/// </param>
+		public HellManager(List<Type> detectedPlugins)
+		{
+			UnloadedTypes = new List<Type>(detectedPlugins);
+		}
 
-        /// <summary>
-        /// Load method will be called after finishing all preparations for
-        /// loading.
-        /// </summary>
-        protected override void Load()
-        {
-            LoadedTypes = new List<Type>();
+		/// <summary>
+		/// Load method will be called after finishing all preparations for
+		/// loading.
+		/// </summary>
+		protected override void Load()
+		{
+			LoadedTypes = new List<Type>();
 
-            Database = new DatabaseConnector();
+			Database = new DatabaseConnector();
 
-            string[] settings = Database.EnumSettings("HellAdapter");
-            // Call new List<Type> here for making copy of list, because list
-            // itself can be changed during plugin loading.
-            foreach (Type type in new List<Type>(UnloadedTypes))
-            {
-                bool hasOption = settings.Contains(type.FullName);
-                if ((hasOption && (byte)Database.GetSetting("HellAdapter",
-                    type.FullName) == 1) || !hasOption)
-                {
-                    LoadType(type);
-                }
-            }
+			string[] settings = Database.EnumSettings("HellAdapter");
+			// Call new List<Type> here for making copy of list, because list
+			// itself can be changed during plugin loading.
+			foreach (Type type in new List<Type>(UnloadedTypes))
+			{
+				bool hasOption = settings.Contains(type.FullName);
+				if ((hasOption && (byte)Database.GetSetting("HellAdapter",
+					type.FullName) == 1) || !hasOption)
+				{
+					LoadType(type);
+				}
+			}
 
-            options = new OptionsPage(HInstance, HLangpack, this);
-        }
+			options = new OptionsPage(HInstance, HLangpack, this);
+		}
 
-        /// <summary>
-        /// Unload method will be called on plugin unloading (for example, on
-        /// Miranda exit).
-        /// </summary>
-        public override void Unload()
-        {
-            options.Dispose();
-            
-            foreach (Plugin plugin in loadedPlugins)
-            {
-                plugin.Unload();
-            }
+		/// <summary>
+		/// Unload method will be called on plugin unloading (for example, on
+		/// Miranda exit).
+		/// </summary>
+		public override void Unload()
+		{
+			if (options != null)
+			{
+				options.Dispose();
+			}
+			
+			foreach (Plugin plugin in loadedPlugins)
+			{
+				plugin.Unload();
+			}
 
-            loadedPlugins.Clear();
-        }
+			loadedPlugins.Clear();
+		}
 
-        /// <summary>
-        /// Loads specified plugin type instance. Moves it from list
-        /// UnloadedTypes into LoadedTypes.
-        /// </summary>
-        /// <param name="pluginType">
-        /// Plugin type; must be inherited from Plugin.
-        /// </param>
-        internal void LoadType(Type pluginType)
-        {
-            var plugin = pluginType.GetConstructor(new Type[0]).Invoke(
-                new object[0]) as Plugin;
-            plugin.Load(HInstance, HLangpack);
+		/// <summary>
+		/// Loads specified plugin type instance. Moves it from list
+		/// UnloadedTypes into LoadedTypes.
+		/// </summary>
+		/// <param name="pluginType">
+		/// Plugin type; must be inherited from Plugin.
+		/// </param>
+		internal void LoadType(Type pluginType)
+		{
+			var plugin = pluginType.GetConstructor(new Type[0]).Invoke(
+				new object[0]) as Plugin;
+			plugin.Load(HInstance, HLangpack);
 
-            loadedPlugins.Add(plugin);
-            if (UnloadedTypes.Contains(pluginType))
-                UnloadedTypes.Remove(pluginType);
-            LoadedTypes.Add(pluginType);
-        }
+			loadedPlugins.Add(plugin);
+			if (UnloadedTypes.Contains(pluginType))
+				UnloadedTypes.Remove(pluginType);
+			LoadedTypes.Add(pluginType);
+		}
 
-        /// <summary>
-        /// Unloads all instances of specified plugins. Moves it from list
-        /// LoadedTypes into UnloadedTypes.
-        /// </summary>
-        /// <param name="pluginType">
-        /// Plugin type; must be inherited from Plugin.
-        /// </param>
-        internal void UnloadType(Type pluginType)
-        {
-            foreach (Plugin plugin in loadedPlugins)
-            {
-                if (plugin.GetType() == pluginType)
-                {
-                    plugin.Unload();
-                }
-            }
+		/// <summary>
+		/// Unloads all instances of specified plugins. Moves it from list
+		/// LoadedTypes into UnloadedTypes.
+		/// </summary>
+		/// <param name="pluginType">
+		/// Plugin type; must be inherited from Plugin.
+		/// </param>
+		internal void UnloadType(Type pluginType)
+		{
+			foreach (Plugin plugin in loadedPlugins)
+			{
+				if (plugin.GetType() == pluginType)
+				{
+					plugin.Unload();
+				}
+			}
 
-            loadedPlugins.RemoveAll(
-                (plugin) => plugin.GetType() == pluginType);
+			loadedPlugins.RemoveAll(
+				(plugin) => plugin.GetType() == pluginType);
 
-            if (LoadedTypes.Contains(pluginType))
-                LoadedTypes.Remove(pluginType);
-            UnloadedTypes.Add(pluginType);
-        }
-    }
+			if (LoadedTypes.Contains(pluginType))
+				LoadedTypes.Remove(pluginType);
+			UnloadedTypes.Add(pluginType);
+		}
+	}
 }
